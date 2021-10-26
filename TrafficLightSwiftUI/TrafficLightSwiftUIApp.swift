ebd641acff1f7ad262872f695ac3bc393f1ea939
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-10-26 11:10:38 +0300

    Initial Commit

---
+++ TrafficLightSwiftUI/TrafficLightSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightSwiftUIApp.swift
+//  TrafficLightSwiftUI
+//
+//  Created by Vahtee Boo on 26.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
